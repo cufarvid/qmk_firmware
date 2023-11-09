5acdd49a05ac4d3c090dd8ba7bca686951c5b669
Vid Čufar <vid@cufar.dev>
2023-11-09 22:11:58 +0100

    Add LCD display support - displays current os, layer and wpm
    Signed-off-by: Vid Čufar <vid@cufar.dev>

--- keyboards/bastardkb/dilemma/4x6_4/keymaps/cufarvid/rules.mk
+++ keyboards/bastardkb/dilemma/4x6_4/keymaps/cufarvid/rules.mk
@@ -1,3 +1,12 @@
 VIA_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
 CAPS_WORD_ENABLE = yes
+OS_DETECTION_ENABLE = yes
+WPM_ENABLE = yes
+
+QUANTUM_PAINTER_ENABLE = yes
+QUANTUM_PAINTER_DRIVERS += gc9a01_spi
+
+SRC += cufarvid.c \
+       display.c \
+       generated/fira-code-nerd.qff.c
